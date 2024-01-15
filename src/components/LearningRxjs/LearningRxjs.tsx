f2c14abac0a91ec8405d8a69cb4ec75848f26fea
AzureAD\SahalAbdullah <sahal.abdullah@aquila360.com>
2024-01-15 13:13:24 +0500

    rxjs basics

--- src/components/LearningRxjs/LearningRxjs.tsx
+++ src/components/LearningRxjs/LearningRxjs.tsx
@@ -2,6 +2,15 @@ import { Observable, from } from 'rxjs';
 import { map, filter } from 'rxjs/operators';
 
 const LearningRxjs = () => {
+  let numbersObservable = from([1, 2, 3, 4, 5]);
+  let squaredNumbers = numbersObservable.pipe(
+    filter((val) => val > 2),
+    map((val) => val * val)
+  );
+  let subscription = squaredNumbers.subscribe((result) => {
+    console.log(result);
+  });
+
   return (
     <div>
       <h2>Rxjs Learning</h2>
